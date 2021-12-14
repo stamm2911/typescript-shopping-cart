5d89ccd881d30a40ae13c093101f6653b54020be
stamm2911 <edstamm29@gmail.com>
2021-12-13 21:16:58 -0600

    fetch data creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
+import { useQuery } from 'react-query'
+
+interface fetchDataType {
+  id: number,
+  category: string,
+  description: string,
+  image: string,
+  price: number,
+  title: string,
+  amount: number,
+}
+
+const url: string = 'https://fakestoreapi.com/products'
+
+const fetchData = async () => {
+  const fetchRestaurantData = await fetch(url)
+  const data = await fetchRestaurantData.json()
+  console.log(data)
+}
+
+fetchData()
+
 function App() {
   return (
-    <div className="App">
+    <div>
       start
     </div>
   );
